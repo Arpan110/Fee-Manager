05074ce4c1192c3b917d3f1befb6523111843123
Ritam Pal <palritam2004@gmail.com>
2026-01-26 08:09:04 +0530

    febicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,25 +10,20 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Student Fee Manager',
+  title: 'Fee Manager',
   description: 'Month-wise Student Fee Management System',
- /* icons: {
+ icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/fevicon.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
+        url: '/fevicon.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
-  },*/
+  },
 }
 
 export default function RootLayout({
